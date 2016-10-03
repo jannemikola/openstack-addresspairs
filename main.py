2b63c3f80195c9123827f4de5638fcfbe6b623e7
Janne Mikola <janne.mikola@outlook.com>
2016-10-03 11:40:35 +0300

    Minor: fixed too long line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Workaround for OpenStack bug in Juno and Kilo releases, where user is not able to remove allowed_address_pairs via GUI (Horizon) 
-# nor CLI after adding them with command-line client. This bug cannot be fixed by updating OpenStack CLI tools, it requires OpenStack 
-# platform upgrade, and such a thing can be impossible depending on your role in the system (e.g. if you are just a tenant). This 
-# is a script to remedy the situation by utilizing OpenStack's RESTful API, which is the only known workaround.
+# nor CLI after adding them with command-line client. This bug cannot be fixed by updating OpenStack CLI tools, it requires 
+# OpenStack platform upgrade, and such a thing can be impossible depending on your role in the system (e.g. if you are just a 
+# tenant). This is a script to remedy the situation by utilizing OpenStack's RESTful API, which is the only known workaround.
 #
 # Tested with HPE CloudSystem 9 OpenStack (Juno), should work with other Juno-based OpenStack variants/distributions as well. Kilo 
 # compatibility unknown, but will verify and make the script adaptive for different versions later.
